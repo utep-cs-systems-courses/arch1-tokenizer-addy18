effa3e69666e5a3aeb6f17a31ccf48340e711f66
Addy <amgalvan@miners.utep.edu>
2021-02-09 22:46:43 -0700

    put a menu and options for interface

--- src/main.c
+++ src/main.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
 
-void main(){
+int main(){
   char str[50];
-
-  printf("Enter a string:\n");
-  printf("> ");
-  fgets(str, 50, stdin);
-
-  printf("%s\n", str);
+  int choice;
   
+  while(1){
+    printf("Select an option: \n");
+    printf("1. tokenize a string\n");
+    printf("2. string history\n");
+    printf("3. exit\n");
+    scanf("%d", &choice);
+    printf("\n");
+
+    if(choice == 1){
+      printf("Enter a string:\n");
+      printf("> ");
+      fgets(str, 50, stdin);
+
+      printf("%s\n", str);
+    }else if(choice == 2){
+      printf("history\n");
+    }else if(choice == 3){
+      printf("Goodbye :)\n");
+      goto done;
+    }
+  }
+    
+  done:
+    return 0;
 }
