1604b9b498ff8be21d1001b6978d31d607391513
Addy <amgalvan@miners.utep.edu>
2021-02-09 21:12:36 -0700

    created a simple UI, repeating user input

--- src/main.c
+++ src/main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
 void main(){
-  printf("hello\n");
+  char str[50];
+
+  printf("Enter a string:\n");
+  printf("> ");
+  fgets(str, 50, stdin);
+
+  printf("%s\n", str);
+  
 }
