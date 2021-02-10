4f3a6b27b9f51dd85fb092b32dea34f6284c822c
Addy <amgalvan@miners.utep.edu>
2021-02-09 22:11:14 -0700

    completed the first three methods of tokenizer

---
+++ src/tokenizer.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include tokenizer.h
+
+// true if c is a tab or space, not zero
+int space_char(char c){
+   if(c == ' ' || c == '\t'){
+      return 1;
+   }
+   return 0;
+}
+
+//true if c not a tab or space, and not zero
+int non_space_char(char c){
+   if(c != ' ' || c != '\t'){
+      return 1;
+   }
+   return 0;
+}
+
+// * to first (non-space) char in first word in s
+char *word_start(char *s){
+   while(space_char(*s)){
+     s++;
+   }
+   return s;
+}
+
+// * to char after end of word
+char *word_terminator(char *word){}
+
+// the number of words in s
+int count_words(char *s){}
