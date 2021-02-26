bd86efa8237bf92c9e2d698388517c8d19c2dc4c
Addy <amgalvan@miners.utep.edu>
2021-02-26 01:30:30 -0700

    finished count words

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -4,7 +4,7 @@
 
 // true if c is a tab or space, not zero
 int space_char(char c){
-   if(c == ' ' || c == '\t'){
+  if((c == ' ' || c == '\t') && c != '\0'){
       return 1;
    }
    return 0;
@@ -12,22 +12,38 @@ int space_char(char c){
 
 //true if c not a tab or space, and not zero
 int non_space_char(char c){
-   if(c != ' ' || c != '\t'){
-      return 1;
+  if((c == ' ' || c == '\t') && c != '\0'){
+      return 0;
    }
-   return 0;
+   return 1;
 }
 
 // * to first (non-space) char in first word in s
-char word_start(char *str){
-   while(space_char(*str)){
-     str++;
-   }
-   return *str;
+char *word_start(char *str){
+   int i = 0;
+   while(space_char(str[i])) i++;
+   printf("start\n");
+   return &str[i];
 }
 
-// * to char after end of word
-char *word_terminator(char *word){}
+// * to char after end of word --- look back 
+char *word_terminator(char *word){
+  int i = 0;
+  while(non_space_char(word[i])) i++;
+ 
+  return &word[i];
+}
 
 // the number of words in s
-int count_words(char *s){}
+int count_words(char *s){
+  int count = 0;
+  int i = 0;
+  while(s[i] != '\0'){
+    if(space_char(s[i]) && non_space_char(s[i+1]))
+       count++;
+       i++;
+  }
+  count++;
+  return count;
+}
+
