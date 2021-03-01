c0f8cded40e84bb74ed3cb0bc569a1c9298f7f23
Addy <amgalvan@miners.utep.edu>
2021-02-28 19:29:43 -0700

    added milestone 2

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -47,3 +47,46 @@ int count_words(char *s){
   return count;
 }
 
+char *copy_str(char *inStr, short len){
+  char *outputString = malloc((len+1) *sizeof(char));
+  int i = 0;
+  while(i <= len){
+    outputString[i] = inStr[i];
+    i++;
+  }
+  return outputString;
+}
+
+char **tokenize(char *str){
+  int index, len;
+  int totalIndex = count_words(str);
+
+  char **tokens = malloc((totalIndex + 1) * sizeof(char*));
+  char *strpointer = str;
+
+  int i;
+  for(i = 0; i < totalIndex; i++){
+    strpointer = word_start(strpointer);
+    len = (word_terminator(strpointer) - word_start(strpointer));
+    tokens[i] = copy_str(strpointer, len);
+    strpointer = word_terminator(strpointer);
+  }
+  tokens[i] = 0;
+  return tokens;
+}
+
+void print_tokens(char **tokens){
+  int index = 0;
+  while(tokens[index] != NULL){
+    printf("%s\n", tokens[index]);
+    index++;
+  }
+}
+
+void free_tokens(char **tokens){
+  int index = 0;
+  for(index = 0; tokens[index] != 0; index++){
+    free(tokens[index]);
+  }
+  free(tokens);
+}
