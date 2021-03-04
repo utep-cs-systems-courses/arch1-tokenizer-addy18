c4ba1aa0a5585beab8823a46768cedcbb93624f8
Addy <amgalvan@miners.utep.edu>
2021-03-04 16:04:56 -0700

    fixed my id counter and added comments throughout my code

--- src/history.c
+++ src/history.c
@@ -3,51 +3,60 @@
 #include "history.h"
 #include "tokenizer.h"
 
-List *init_history(){
-  List *list = malloc(sizeof(List));
-  list->root = malloc(sizeof(Item));
+List* init_history(){
+  List *list = malloc(sizeof(List)); //allocated memory for linked list
+  list->root = NULL; //empty linked list
   return list;
 }
 
 void add_history(List *list, char *str){
-  int id = 1;
-  Item *node = list -> root;
-
-  while(node -> next != NULL){
-    node = node -> next;
-    id++;
+  int count = 2; //placement in list
+  if(list->root == NULL){
+    Item *newNode = malloc(sizeof(Item));
+    newNode->str = copy_str(str, (word_terminator(str) - word_start(str))); //copy string into new node
+    newNode->id = 1;//inital place, first node
+    newNode->next = NULL;
+    list->root = newNode;
+  }else{
+    Item *current = list->root;
+    while(current->next != NULL){
+      current = current->next;
+      count += 1;
+    }
+    current->next = malloc(sizeof(Item));
+    current->next->str = copy_str(str, (word_terminator(str) - word_start(str)));
+    current->next->id = count;
+    current->next->next = NULL;
   }
-  node -> next = malloc(sizeof(Item));
-  node -> next -> str = copy_str(str, (word_terminator(str) - word_start(str)));
-  node -> next -> id = id;
 }
 
 char *get_history(List *list, int id){
-  int curr_id = 0;
-  Item *temp = list -> root;
-  while(temp -> next != NULL){
-    if(curr_id = temp -> id){
-      break;
+  if(list->root == NULL){
+    printf("list in empty");
+  }else{
+    Item *current = list->root; //traverse through the list
+    while(current->next != NULL){
+      if(current->id == id){ //if id matches current id return str 
+	return current->str;
+      }
+      current = current->next; //set node to next
     }
-    temp = temp -> next;
-    curr_id++;
   }
-  return temp -> str;
 }
 
-
 void print_history(List *list){
-  Item *temp = list -> root;
+  Item *temp = list->root;
   while(temp != NULL){
-    printf("ID: %d || %s\n", temp -> id, temp -> str);
-    temp = temp -> next;
+    printf("ID: %d || %s\n", temp->id, temp->str);
+    temp = temp->next;
   }
 }
 
 void free_history(List *list){
-  Item *temp = list -> root;
+  Item *temp = list->root;
+
   while(temp != NULL){
-    Item *tempNext = temp -> next;
+    Item *tempNext = temp->next;
     free(temp);
     temp = tempNext;
   }
